294f03cbd9bc4b76e6bff986573cb0083aca1198
lilliannguyen <lillian.nguyen@baltimorecity.gov>
2021-11-10 15:11:22 -0500

    Streamline export messages

--- expProjections/R/1_export.R
+++ expProjections/R/1_export.R
@@ -25,10 +25,9 @@ export_projections_tab <- function(agency_id, list) {
     style$rows <- 2:nrow(data$line.item)
 
     # custom formatting for projections tab
-    excel <- data$line.item %>%
-      export_excel(
+    excel <- suppressMessages(export_excel(data$line.item,
         "Projection", data$file, "new", table_name = "projection",
-        col_width = rep(15, ncol(.)), save = FALSE)
+        col_width = rep(15, ncol(data$line.item)), save = FALSE))
     dataValidation(
       excel,  1, rows = style$rows, type = "list", value = "Calcs!$A$2:$A$10",
       cols = grep(internal$col.calc, names(data$line.item)))
@@ -48,7 +47,9 @@ export_projections_tab <- function(agency_id, list) {
     saveWorkbook(excel, data$file, overwrite = TRUE)
 
     # needed for validation of Qx Calculation column
-    export_excel(calc.list, "Calcs", data$file, "existing", show_tab = FALSE)
+    suppressMessages(export_excel(calc.list, "Calcs", data$file, "existing", show_tab = FALSE))
+    
+    message(data$agency, " Projections tab exported.")
 
   },
 
@@ -97,10 +98,10 @@ export_pivot_tabs <- function(agency_id, list) {
 
       style$rows <- 2:(nrow(data[[tolower(type)]]) + 1)
 
-      excel <- export_excel(data[[tolower(type)]],
+      excel <- suppressMessages(export_excel(data[[tolower(type)]],
                             paste0("Pivot-", type), data$file, "existing", table_name = type,
                             col_width = c(rep("auto", 4), rep(15, ncol(data[[tolower(type)]]) - 4)),
-                            save = FALSE)
+                            save = FALSE))
       addStyle(
         excel, paste0("Pivot-", type), style$cell.bg, rows = 1,
         gridExpand = TRUE, stack = FALSE,
@@ -128,11 +129,10 @@ export_pivot_tabs <- function(agency_id, list) {
     style$cols <- grep(paste0("Object ", 0:9, collapse = "|"),
                        names(data$program.surdef))
 
-    excel <- data$program.surdef %>%
-      export_excel(
+    excel <-  suppressMessages(export_excel(data$program.surdef,
         "Pivot-Service SurDef", data$file, "existing",
         col_width = c("auto", 40, "auto", "auto", rep(15, ncol(data$program.surdef) - 4)),
-        save = FALSE)
+        save = FALSE))
     conditionalFormatting(
       excel, "Pivot-Service SurDef", cols = style$cols, rows = style$rows,
       style = style$negative, type = "expression", rule = "<0")
@@ -143,6 +143,8 @@ export_pivot_tabs <- function(agency_id, list) {
       excel, "Pivot-Service SurDef", style$total,
       cols = 1:max(style$cols), rows = max(style$rows), gridExpand = TRUE, stack = TRUE)
     saveWorkbook(excel, data$file, overwrite = TRUE)
+    
+    message(data$agency, " Pivot tabs exported.")
 
   },
 
